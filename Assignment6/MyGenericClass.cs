70b345425bdb19a00354c21e497f62c37a3ae370
DiandraKarina <ghermandiandra@yahoo.com>
2022-11-12 19:10:47 +0200

    Assignment6

---
+++ Assignment6/MyGenericClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    internal class MyGenericClass
+    {
+        public class MyGenericCollection<T>
+        {
+            private T[] array;
+            private int next = 0;
+
+            // get item at given index
+            public T Get(int i)
+            {
+                return array[i];
+            }
+
+            // set item at given index method
+            public void Set(int i, T item)
+            {
+                array[i] = item;
+            }
+
+            public MyGenericCollection(int length)
+            {
+                array = new T[length];
+            }
+            
+            //add method
+            public void Add(T item)
+            {
+                array[next] = item;
+                next++;
+            }
+
+            // Swap
+            public void SwapItems(int indexA, int indexB)
+            {
+                T tmp = array[indexA];
+                array[indexA] = array[indexB];
+                array[indexB] = tmp;
+                
+            }
+
+        }
+    }
+}
---
+++ Assignment6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Assignment6;
+
+class Program : MyGenericClass {
+
+    static void Main(string[] args)
+    {
+        MyGenericCollection<string> myColl = new MyGenericCollection<string>(5);
+        myColl.Add("sugar");
+        myColl.Add("pumpkin");
+        myColl.Add("cacao");
+        myColl.Add("coconut");
+        myColl.Add("spices");
+
+
+        for (int i = 0; i < 5; i++)
+       
+            {
+            Console.WriteLine("Item {0}: {1}",i,myColl.Get(i));
+        }
+
+        // Swap two items
+        Console.WriteLine("Unswapped items: {0},{1} ",myColl.Get(3), myColl.Get(4));
+        
+        myColl.SwapItems(3, 4);
+
+        Console.WriteLine("Swapped items: {0},{1} ", myColl.Get(3), myColl.Get(4));
+
+    }
+}
