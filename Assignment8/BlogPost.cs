9047f35cbc2f908921e84d63de848b6f4b96d721
DiandraKarina <ghermandiandra@yahoo.com>
2022-11-25 15:47:23 +0200

    update

---
+++ Assignment8/BlogPost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assignment8
+{
+    public class BlogPost
+    {
+        public BlogPost(string categoryName, string recommendedTime, string blogName)
+        {
+            CategoryName = categoryName;
+            RecommendedTime=recommendedTime;
+            BlogName = blogName;
+        }
+
+        public string BlogName { get; set; }
+        public string CategoryName { get; set; }
+        public string RecommendedTime { get; set; }
+
+    }
+}
+
--- Assignment8/Program.cs
+++ Assignment8/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Assignment8;
+using System.IO.Compression;
+using System.Security.Cryptography;
 using System.Text.Json;
 using static System.Console;
 
@@ -6,52 +8,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        string inputString = "Hello World!";
-        byte[] compressed = GZip.CompressString(inputString);
-        string decompressedString = GZip.DecompressToString(compressed);
+        string mealplan = @"F:\Amdaris\File System&Streams\Assignment8\MealPlan.txt";
 
-        WriteLine($"Original:{inputString}");
-        WriteLine($"Compressed:{JsonSerializer.Serialize(compressed)}");
-        WriteLine($"Decompressed:{decompressedString}");
+        List<BlogPost> blogpost = new List<BlogPost>();
+
+        using (StreamReader sr = new StreamReader(mealplan))
+        {
+            while (sr.EndOfStream == false)
+            {
+                string[] line = sr.ReadLine().Split(' ');
+                if (line[1].Contains("PM"))
+                {
+                    blogpost.Add(new BlogPost(line[0], line[1], line[2]));
+                }
+            }
 
 
-        string inputFile = @"C:\Users\Diandra\Desktop\Assignment8Test.txt";
-        string gzipFile = @"C:\Users\Diandra\Desktop\Assignment8Test.txt.gz";
+            StreamWriter sw = new StreamWriter(@"F:\Amdaris\File System&Streams\Assignment8\diet.txt");
+            foreach (var b in blogpost)
+            {
+                sw.WriteLine(b.RecommendedTime + " " + b.BlogName);
+            }
+        }
+        /*
+        //Encrypt
 
-        if (File.Exists(inputFile)) { File.Delete(inputFile); }
-        if (File.Exists(gzipFile)) { File.Delete(gzipFile); }
-        File.WriteAllText(inputFile, "Write in my file");
-        GZip.CompressFile(inputFile);
+        Aes aes = Aes.Create();
+        
+        FileStream fs = new FileStream(@"F:\Amdaris\File System&Streams\Assignment8\diet.txt", FileMode.OpenOrCreate);
+        FileStream fsEncrypt = new FileStream(@"F:\Amdaris\File System&Streams\Assignment8\diet_encrypted.txt", FileMode.OpenOrCreate);
+        CryptoStream cs = new CryptoStream(fsEncrypt, aes.CreateEncryptor(), CryptoStreamMode.Write);
+                        
+        fs.CopyTo(cs);
+        
+        
+        //GZip
 
-        WriteLine($"Original:{File.ReadAllText(inputFile)}");
-        WriteLine($"Compressed:{JsonSerializer.Serialize(File.ReadAllBytes(gzipFile))}");
-        File.Delete(inputFile);
-        GZip.DecompressFile(gzipFile);
-        WriteLine($"Decompressed:{File.ReadAllText(inputFile)}");
+        FileStream fsCompress = new FileStream(@"F:\Amdaris\File System&Streams\Assignment8\diet_compressed.txt", FileMode.OpenOrCreate);
+        GZipStream gs = new GZipStream(fsCompress, CompressionMode.Compress);
+                
+        fs.CopyTo(gs);
        
+        //UnGzip
+        
+        using (FileStream reader = File.OpenRead(@"F:\Amdaris\File System&Streams\Assignment8\diet_compressed.txt"))
+        using (GZipStream zip = new GZipStream(reader, CompressionMode.Decompress, true))
+        using (StreamReader unzip = new StreamReader(zip))
+        while (!unzip.EndOfStream)
+        unzip.ReadLine();
 
-        try
-        {
-            string FileName = "test.txt";
+        //decrypt
+        
+        FileStream fs2 = new FileStream(@"F:\Amdaris\File System&Streams\Assignment8\diet_encrypted.txt", FileMode.Open);
+        CryptoStream cs2 = new CryptoStream(fs2, aes.CreateDecryptor(), CryptoStreamMode.Read);
+        StreamReader reader2 = new StreamReader(cs2);
+        string content=reader2.ReadToEnd();
+        Console.WriteLine(content);
+        
+        */
 
-            Console.WriteLine("Encrypt " + FileName);
 
-            // Encrypt the file.
-            Encryption.AddEncryption(FileName);
 
-            Console.WriteLine("Decrypt " + FileName);
 
-            // Decrypt the file.
-            Encryption.RemoveEncryption(FileName);
 
-            Console.WriteLine("Done");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-        }
 
-        Console.ReadLine();
+
     }
 }
 
